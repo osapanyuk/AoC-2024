3d9c0444b1749be6d45d76754313b2d8f65f9d30
osapanyuk <oleg@nexthopself.ca>
2025-03-10 06:16:44 +0000

    day01

---
+++ day01/go.mod
@@ -0,0 +1,3 @@
+module github.com/osapanyuk/day01
+
+go 1.24.1
--- day01/main.go
+++ day01/main.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+	_ "embed"
+    "fmt"
+	"log"
+    "sort"
+    "strconv"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+func init() {
+    if len(input) == 0 {
+        log.Fatal("Input file is empty... Rebuild is necessary")
+    }
+    input = strings.TrimRight(input, "\n")
+}
+
+func abs(val int) int {
+    if val < 0 {
+        return -val
+    }
+    return val
+}
 
 func main() {
-    fmt.Println("hello world")
+    listOne := []int{}
+    listTwo := []int{}
+    for _, numLine := range strings.Split(input, "\n") {
+        splitNums := strings.Fields(numLine)
+
+        numOne, err := strconv.Atoi(splitNums[0])
+        if err != nil {
+            log.Fatal("Input file contains non-numerical values")
+        }
+
+        listOne = append(listOne, numOne)
+
+        numTwo, err := strconv.Atoi(splitNums[1])
+        if err != nil {
+            log.Fatal("Input file contains non-numerical values")
+        }
+
+        listTwo = append(listTwo, numTwo)
+    }
+
+    sort.Ints(listOne)
+    sort.Ints(listTwo)
+
+    diff := 0
+
+    for i := range len(listOne) {
+        diff += abs(listOne[i] - listTwo[i])
+    }
+
+    fmt.Println("The distance is ", diff)
 }
